9a2d40ab60e40e57127801a0052dd3fe82cf7e18
degnemark <mbv555.alumni.ku.dk>
2024-06-08 14:20:04 +0200

    changes in README and remove out commented code

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ Assumes a working Python 3 installation (with python=python3 and pip=pip3).
 (2) Initialize database: 
 IMPORTANT: In 'Results Attributes.SQL' change the full path of the 'results.csv' file which is located in the tmp directory
 
->$ psql -h hostname -d dbname -U username -W
-or
+RUN:
 >$ psql -U dbownername -d dbname
 
 Create table users:
@@ -21,5 +20,21 @@ Create tabel 'results' and populate data:
 (4) Run web-app:
 
 >$ python src/app.py
+
+----------------------------------------------------------------------------------------------
+
+# How to use the application:
+
+(1) Create account / You start by pressing the 'Create Account' button, you then get to page where you choose your username and password. The username must start with a capital letter, and the password must have atleast one special character (!@#$%^&*(),.?":{}|<>_)
+
+(2) Login / Now you can login on your account by typing in your username and password.
+
+(3) Frontpage / you see two fields, one for home team and one for away team - these country names must start with a capital letter - click search - all matches with given teams as home and away team will be visible underneath (e.g. home team = "Denmark" and away team "Sweden", click search - will show all matches, where Denmark played against Sweden as home team)
+
+(4) Profile / Here you can update your accounts password (still required to have at least one special character) - will transfer you to the update password page - you can also delete your profile, and be retransfered to the login screen 
+
+
+
+
 
 
--- src/app.py
+++ src/app.py
@@ -12,8 +12,8 @@ import random
 
 app = Flask(__name__ , static_url_path='/static')
 
-# set your own database name, username and password. IMPORTANT: If wrong database will crash!
-db = "dbname='Footballpage' user='oliverlarsen' host='localhost' password=''" #potentially wrong password
+# set your own database name, username and password
+db = "dbname='XXXX' user='XXXX' host='localhost' password='XXXX'" #potentially wrong password
 conn = psycopg2.connect(db)
 cursor = conn.cursor()
 
@@ -82,83 +82,6 @@ def createaccount():
     return render_template("createaccount.html")
 
 
-
-
-# @app.route("/", methods=["POST", "GET"])
-# def home():
-#     cur = conn.cursor()
-#     #Getting 10 random rows from Attributes
-#     tenrand = '''select * from Attributes order by random() limit 10;'''
-#     cur.execute(tenrand)
-#     punks = list(cur.fetchall())
-#     length = len(punks)
-
-#     #Getting random id from table Attributes
-#     randint = '''select id from Attributes order by random() limit 1;'''
-#     cur.execute(randint)
-#     randomNumber = cur.fetchone()[0]
-#     if not session.get('logged_in'):
-#         return render_template('login.html')
-#     else:
-#         if request.method == "POST":
-#             input_gender = request.form["radio"].lower()
-#             input_type = request.form["radiotype"].lower()
-#             input_skin = request.form["radioskin"].lower()
-
-#             input_count = request.form["accessCount"] or -1
-#             input_access = request.form["access"].lower() or "NaN"
-
-#             input_id = request.form["punkid"].lower() or ""
-
-#             if input_id != "":
-#                 input_id = input_id.zfill(4)
-#                 return redirect(url_for("punkpage", punkid=input_id))
-#             return redirect(url_for("querypage", gender=input_gender, types=input_type, skin=input_skin, access=input_access, count=input_count))
-            
-#         length = len(punks)
-#         return render_template("index.html", content=punks, length=length, randomNumber = randomNumber)
-
-# @app.route("/punks/<gender>/<types>/<skin>/<count>/<access>")
-# def querypage(gender, types, skin, count, access):
-#     cur = conn.cursor()
-#     rest = 0
-
-#     sqlcode = f'''select * from Attributes where '''
-#     if gender != "both":
-#         sqlcode += f''' gender = '{gender}' and'''
-#         rest += 1
-
-#     if types != "all":
-#         sqlcode += f''' type = '{types}' and'''
-#         rest += 1
-
-#     if skin != "all":
-#         sqlcode += f''' skin_tone = '{skin}' and'''
-#         rest += 1
-
-#     if access != "NaN":
-#         rest += 1
-#         sqlcode += f''' accessories ~* '{access}' and'''
-    
-#     if int(count) != -1:
-#         rest += 1
-#         sqlcode += f''' count = '{count}' and'''
-
-#     if rest == 0: 
-#         sqlcode = f''' select * from Attributes'''
-
-#     else: 
-#         sqlcode  = sqlcode[:-3]
-
-#     cur.execute(sqlcode)
-#     ct = list(cur.fetchall())
-
-
-#     length = len(ct)
-
-#     return render_template("cryptoquery.html", content=ct, length=length)
-
-
 @app.route('/login', methods=['POST'])
 def do_admin_login():
     cur = conn.cursor()
@@ -179,10 +102,6 @@ def do_admin_login():
     return redirect(url_for("home"))
 
 
-# @app.route("/contact")
-# def contact():
-#     return render_template("contact.html")
-
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
@@ -196,19 +115,6 @@ def profile():
     
     username = session['username']
 
-    # # SQL for updating password
-    # new_password = request.form['password']
-    # updPassSql = f'''UPDATE users SET password = '{new_password}' WHERE username = '{username}' '''
-
-    # # SQL for deleting profile 
-    # delProfSql = f'''DELETE FROM users WHERE username = '{username}' '''
-
-    ########### Old code 
-    #sql1 = f'''select id, type, gender, skin_tone, count, accessories from favorites natural join attributes where username = '{username}' '''
-    # cur.execute(sql1)
-    # favs = cur.fetchall()
-    #length = len(favs)
-    #return render_template("profile.html", content=favs, length=length, username = username)
     return render_template("profile.html", username=username)
 
 @app.route("/updpass", methods=['POST', 'GET'])
@@ -216,7 +122,6 @@ def updpass():
     cur = conn.cursor()
     if not session.get('logged_in'):
         return render_template('login.html')
-        #return redirect(url_for('/login', next=url_for('/updpass')))
     else:
         if request.method == 'POST':
             new_password = request.form['password']
@@ -246,50 +151,8 @@ def deleteuser():
     
     session['logged_in'] = False
 
-    # return render_template('login.html')
     return home()
     
-
-
-# @app.route("/punk/<punkid>", methods=["POST", "GET"])
-# def punkpage(punkid):
-#     cur = conn.cursor()
-#     """
-#     Instead of PunkID we would have our database content
-#     for 1 cryptopunk instead.
-#     """
-#     if not session.get('logged_in'):
-#         return render_template('login.html')
-
-#     if request.method == "POST":
-#         # Add til favourite
-#         username = session['username']
-#         try: 
-#             sql1 = f'''insert into favorites(id, username) values ('{punkid}', '{username}') '''
-#             cur.execute(sql1)
-#             conn.commit()
-#         except:
-#             conn.rollback()
-
-
-
-#     req = "https://cryptopunks.app/cryptopunks/details/"+ punkid
-#     response = requests.get(req)
-#     soup = BeautifulSoup(response.content, 'html.parser')
-#     rows = soup.select("table.ms-rteTable-default tr")
-#     pricelist = str(soup.find(class_="punk-history-row-bid")).split('\n')
-#     if len(pricelist) < 5:
-#         price = "10Ξ ($18,000)"
-#     else:
-#         price =pricelist[4].replace('</td>', '').replace('<td>','')
-
-#     sql1 = f''' select * from attributes where id = '{punkid}' '''
-
-#     cur.execute(sql1)
-
-#     ct = cur.fetchone()
-
-#     return render_template("cryptopunk.html", content=ct, price=price)
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
